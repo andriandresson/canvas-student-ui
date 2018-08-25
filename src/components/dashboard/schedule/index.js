d7333c2f15851649ea863ec0089caf17e3417b35
Andri Andresson <andriandresson@gmail.com>
2018-08-25 21:06:43 +0000

    Schedule - Component created

---
+++ src/components/dashboard/schedule/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ContentItem from '../contentItem';
+
+class Schedule extends Component {
+    render(){
+        return (
+            <ContentItem>
+                <p>Schedule</p>
+            </ContentItem>
+        )
+    }
+}
+
+export default Schedule;
