9e3b8412354019fc0cc20b69cfdafff2183acde8
Andri Andresson <andriandresson@gmail.com>
2018-08-25 21:01:18 +0000

    index.js
    To make sure a container with the same id wont exist we create new
    element with id generetated by objectId method.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ObjectId from './utilities/objectId';
+import Store from './store';
+const rootElement = document.createElement("div"), rootElementID = ObjectId();
+rootElement.id = rootElementID;
+document.body.appendChild(rootElement);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={Store} />, document.getElementById(rootElementID));
 registerServiceWorker();
